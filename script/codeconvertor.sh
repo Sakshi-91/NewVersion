5eb14cbce66b9cc624a9ed99c94b859935c34130
Sakshi-Git1 <144549342+Sakshi-Git1@users.noreply.github.com>
2023-10-05 12:36:20 +0530

    Update codeconvertor.sh

--- script/codeconvertor.sh
+++ script/codeconvertor.sh
@@ -7,55 +7,73 @@ a="sk-ZUWiA4g2eg45J"
 b="2kt8yJYT3BlbkFJQ"
 c="2kurugfZgqjBrju45mO"
 api_key=$a$b$c
-
-prompt_file_path="./script/prompt.txt"
-prompt_contents=$(<"$prompt_file_path")
-
+api_key=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
 curl --silent $url \
 		-H "Authorization: Bearer $api_key" \
 		-H "Content-Type: application/json" \
 		-d "{
 		     \"model\": \"gpt-3.5-turbo\",
-	    	     \"messages\": [{\"role\": \"user\", \"content\": \"$prompt_contents\n$1\"}],
-		     \"temperature\": 0.7
-	            }" \
-		|awk -F\" '
-		$2=="content" {
-				print $0
-				}' \
-		|awk -F\"content\": '
-			{
-			   print $2
-		   	}' 
+	    	     \"messages\": [{\"role\": \"user\", \"content\": \"$1\n$2\"}],
+		     \"temperature\": 0.4
+	            }" 
+		#|awk -F\" '
+		#$2=="content" {
+		#		print $0
+		#		}' \
+		#|awk -F\"content\": '
+		#	{
+		#	   print $2
+		#   	}' 
 }
-​
-#url=https://jsonplaceholder.typicode.com/posts
 url=https://api.openai.com/v1/chat/completions
-[[ $1 == *.cs* ]] || [[ $1 == *.CS* ]] && file_list=($*) && zip_count=1 #we have set zip_count is 1 to iterate outter for loop atleast one time
-file_list=2
-zip_count=1
-[[ $1 == *.zip* ]] || [[ $1 == *.ZIP* ]] && zip_flag=1 && zip_count=$# 
-for j in `seq $zip_count`
+repo_url=$1
+repo_name=`echo $repo_url|awk -F'/' '{printf "%s",$NF}'`
+repo_name=GetAccountDetails
+echo $repo_name
+cd $repo_name
+file_list=(`find . -name *.cs|grep -vE '/obj/Debug/|/obj/Release/|/Properties/|/obj/Debug/'`)
+cd ..
+echo ${file_list[*]}
+prompt=`cat ./prompt/prompt.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
+echo $prompt
+cd $repo_name
+file_content=`cat ${file_list[*]}|grep -v '//'|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
+cd ..
+​
+mkdir -p code_conversion
+cd code_conversion
+#convert_code "$prompt" "$file_content" > input.json
+#java_code=$(jq -r '.choices[].message.content' input.json)
+java_code=$(convert_code "$prompt" "$file_content"|jq -r '.choices[].message.content')
+echo "$java_code" > input.json
+# Remove escape characters for double quotes
+java_code=$(echo "$java_code" | sed 's/\\"/"/g')
+# Create directory for Java files
+mkdir -p java_files
+file_names=$(grep -F .java: <<< "$java_code"|tr : ' '|cut -d. -f1)
+echo ${file_names[*]}
+# Use awk to split Java code into individual files
+indx=0
+awk -v RS="\`\`\`java" -v ORS="" -v ARR="${file_names[*]}" 'BEGIN {split(ARR,FILE_NAME," ")} NR > 1 {print $0 > FILE_NAME[NR-1] ".java"}' <<< "$java_code"
+#cd java_files
+for i in *.java
 do
-	[[ $zip_flag ]] && zip_file=$(eval echo "\${$j}") && file_list=(`unzip -o $zip_file|grep '.cs'|awk -F: '{print $2}'|tr '\n' ' '`) && echo Processing $zip_file :-
-	for i in ${file_list[*]}
-	do
-		i="./GetAccountDetails/GetAccountDetails/AccountDetails.cs"
-		#echo $i
-		java_file=`echo $i|cut -d. -f1`
-		java_file=${java_file}.java
-		java_list+=($java_file)
-		echo converting $i 
-		file=`cat $i|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
-		#echo $file
-		code=`convert_code "$file"|cut -c 3-`
-		len=$((${#code}-1))
-		#convert_code "$file"|sed -e 's/\\n/\n/g' -e 's/\\\"/\"/g' #> $java_file
-		echo ${code:0:len}|sed -e 's/\\n/\n/g' -e 's/\\\"/\"/g' > './AccountDetails/src/main/java/com/example/accountdetails/AccountResponse.java'
-		echo ${code:0:len}|sed -e 's/\\n/\n/g' -e 's/\\\"/\"/g' 
-		[[ $zip_flag ]] && rm $i # if *.cs files are extracted from zip, so after conversion we can delete them because they are already present in zip 
-	done
-	[[ $zip_flag ]] && zip -q ${zip_file%.zip}_java.zip ${java_list[*]} && rm ${java_list[*]} # after compressing java files into zip, we can delete them because they are available in zip file.
-	unset java_list
-	echo 
+	pure_code=$(cat $i |awk -v RS="\`\`\`" NR==1'{print $0}')
+	echo "$pure_code" > $i
 done
+awk -v RS="\`\`\`xml" -v ORS="" 'NR > 1 {print $0 > "pom.xml"}' <<< "$java_code"
+pom=$(cat pom.xml|awk -v RS="\`\`\`" 'NR==1 {print $0}')
+git_cmd=$(cat pom.xml|awk -v RS="\`\`\`" 'NR%2!=0 && NR!=1 {print $0}')
+echo "$git_cmd" > git.txt
+grep git git.txt
+if [ $? -ne 0 ]
+then
+	git_cmd=$(cat pom.xml|awk -v RS="\`\`\`" 'NR>1 {print $0}')
+	echo "$git_cmd"|awk -v FS="\`" '{print $2}'|grep '^git' > git.txt
+fi
+echo "$pom" > pom.xml
+bash git.txt
+​
+		#echo ${code:0:len}|sed -e 's/\\n/\n/g' -e 's/\\\"/\"/g' 
+		#[[ $zip_flag ]] && rm $i # if *.cs files are extracted from zip, so after conversion we can delete them because they are already present in zip 
+	#[[ $zip_flag ]] && zip -q ${zip_file%.zip}_java.zip ${java_list[*]} && rm ${java_list[*]} # after compressing java files into zip, we can delete them because they are available in zip file.
