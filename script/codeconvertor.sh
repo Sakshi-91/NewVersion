542f09f6e0c7001f2678f665a337adfccd894b2a
Sakshi-Git1 <144549342+Sakshi-Git1@users.noreply.github.com>
2023-10-05 17:33:44 +0530

    Update codeconvertor.sh

--- script/codeconvertor.sh
+++ script/codeconvertor.sh
@@ -26,18 +26,19 @@ curl --silent $url \
 }
 url=https://api.openai.com/v1/chat/completions
 repo_url=$1
-pwd
 repo_name=`echo $repo_url|awk -F'/' '{printf "%s",$NF}'`
-repo_name="GetAccountDetails"
+repo_name=GetAccountDetails
 echo $repo_name
 cd $repo_name
 file_list=(`find . -name *.cs|grep -vE '/obj/Debug/|/obj/Release/|/Properties/|/obj/Debug/'`)
-file_content=`cat ${file_list[*]}|grep -v '//'|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
 cd ..
 echo ${file_list[*]}
-prompt=`cat ./script/prompt.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
+prompt=`cat ./prompt/prompt.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
 echo $prompt
-
+cd $repo_name
+file_content=`cat ${file_list[*]}|grep -v '//'|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
+cd ..
+​
 mkdir -p code_conversion
 cd code_conversion
 convert_code "$prompt" "$file_content" > input.json
@@ -73,4 +74,11 @@ then
 	echo "$git_cmd"|awk -v FS="\`" '{print $2}'|grep '^git' > git.txt
 fi
 echo "$pom" > pom.xml
-bash git.txt
+mv *.java ../AccountDetails/src/main/java/com/example/accountdetails/
+mv pom.xml ../AccountDetails/
+#spring init --build=maven --dependencies=web  ${repo_name}_java
+#bash git.txt
+​
+		#echo ${code:0:len}|sed -e 's/\\n/\n/g' -e 's/\\\"/\"/g' 
+		#[[ $zip_flag ]] && rm $i # if *.cs files are extracted from zip, so after conversion we can delete them because they are already present in zip 
+	#[[ $zip_flag ]] && zip -q ${zip_file%.zip}_java.zip ${java_list[*]} && rm ${java_list[*]} # after compressing java files into zip, we can delete them because they are available in zip file.
