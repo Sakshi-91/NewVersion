bcf3b05e6f8e35abe0eb49e9fb7d36ef65a2bbe2
Sakshi-Git1 <144549342+Sakshi-Git1@users.noreply.github.com>
2023-10-07 16:41:49 +0530

    Update WebService1.java

--- code_conversion/GetAccountDetails/src/main/java/com/GetAccountDetails/WebService1.java
+++ code_conversion/GetAccountDetails/src/main/java/com/GetAccountDetails/WebService1.java
@@ -4,65 +4,24 @@ package com.GetAccountDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WebService1 {
+    private static final String BACKEND_URL = "http://backend-url.com";
 
-    @GetMapping("/GetAccountDetails/{AccountNumber}")
-    public AccountResponse getAccountDetails(@PathVariable String AccountNumber) {
+    @GetMapping("/GetAccountDetails/{accountNumber}")
+    public AccountResponse getAccountDetails(@PathVariable String accountNumber) {
         String readTask = null;
         AccountResponse accountResponse = new AccountResponse();
-        RestTemplate restTemplate = new RestTemplate();
 
-        String soapRequest = "<soapenv:Envelope xmlns:soapenv=\\"http://schemas.xmlsoap.org/soap/envelope/\\" " +
-                "xmlns:get=\\"http://example.com/GetAccountDetails/\\">" +
-                "<soapenv:Header/>" +
-                "<soapenv:Body>" +
-                "<get:GetAgreementDetailsRequest>" +
-                "<get:AgreementNumber>" + AccountNumber + "</get:AgreementNumber>" +
-                "</get:GetAgreementDetailsRequest>" +
-                "</soapenv:Body>" +
-                "</soapenv:Envelope>";
+        // Make HTTP request to backend URL
+        // ...
 
-        String url = "http://backend-url/GetAgreementDetails";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_XML);
-        HttpEntity<String> request = new HttpEntity<>(soapRequest, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            readTask = response.getBody();
-            XDocument soapResponseXml = XDocument.Parse(readTask);
-
-            String AgreementType = soapResponseXml.Descendants(soapNamespace + "Body")
-                    .Descendants(dataNamespace + "AgreementType")
-                    .FirstOrDefault()?.Value;
-            String Balance = soapResponseXml.Descendants(soapNamespace + "Body")
-                    .Descendants(dataNamespace + "Balance")
-                    .FirstOrDefault()?.Value;
-            String AddressLine1 = soapResponseXml.Descendants(soapNamespace + "Body")
-                    .Descendants(dataNamespace + "AddressLine1")
-                    .FirstOrDefault()?.Value;
-            String AddressLine2 = soapResponseXml.Descendants(soapNamespace + "Body")
-                    .Descendants(dataNamespace + "AddressLine2")
-                    .FirstOrDefault()?.Value;
-            String Country = soapResponseXml.Descendants(soapNamespace + "Body")
-                    .Descendants(dataNamespace + "Country")
-                    .FirstOrDefault()?.Value;
-            String PostalCode = soapResponseXml.Descendants(soapNamespace + "Body")
-                    .Descendants(dataNamespace + "PostalCode")
-                    .FirstOrDefault()?.Value;
-            String Phone = soapResponseXml.Descendants(soapNamespace + "Body")
-                    .Descendants(dataNamespace + "Phone")
-                    .FirstOrDefault()?.Value;
-
-            accountResponse.setAccountNumber(AccountNumber);
-            accountResponse.setAccountType(AgreementType);
-            accountResponse.setAccountBalance(Balance);
-            accountResponse.setAddress(new Address(AddressLine1, AddressLine2, Country, PostalCode));
-            accountResponse.setPhone(Phone);
-        }
+        // Parse response and populate accountResponse object
+        // ...
 
         return accountResponse;
     }
