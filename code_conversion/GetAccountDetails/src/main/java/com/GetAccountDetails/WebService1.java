bebcdd94af639b8924f30225f39bcde8f4484996
Sakshi-Git1 <agrasaks.in.ibm.com>
2023-10-07 16:24:21 +0530

    'AIGeneratedFiles'

--- code_conversion/GetAccountDetails/src/main/java/com/GetAccountDetails/WebService1.java
+++ code_conversion/GetAccountDetails/src/main/java/com/GetAccountDetails/WebService1.java
@@ -1,51 +1,21 @@
 
 package com.GetAccountDetails;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @RestController
 public class WebService1 {
 
-    @GetMapping("/GetAccountDetails/{accountNumber}")
-    public AccountResponse getAccountDetails(@PathVariable String accountNumber) {
-        String soapRequest = "<soapenv:Envelope xmlns:soapenv=\\"http://schemas.xmlsoap.org/soap/envelope/\\" xmlns:get=\\"http://example.com/GetAgreementDetails/\\">\
-" +
-                "    <soapenv:Header/>\
-" +
-                "    <soapenv:Body>\
-" +
-                "        <get:GetAgreementDetailsRequest>\
-" +
-                "            <get:AgreementNumber>" + accountNumber + "</get:AgreementNumber>\
-" +
-                "        </get:GetAgreementDetailsRequest>\
-" +
-                "    </soapenv:Body>\
-" +
-                "</soapenv:Envelope>";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_XML);
-        HttpEntity<String> requestEntity = new HttpEntity<>(soapRequest, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        String soapResponse = restTemplate.exchange("http://backend-url/GetAgreementDetails", HttpMethod.POST, requestEntity, String.class).getBody();
-
-        // Parse the soapResponse and extract the required values
-        // ...
-
+    @GetMapping("/GetAccountDetails")
+    public AccountResponse getAccountDetails(@RequestParam String AccountNumber) {
+        String readTask = null;
         AccountResponse accountResponse = new AccountResponse();
-        // Set the values to accountResponse
-        // ...
+
+        // Your SOAP request logic here
 
         return accountResponse;
     }
