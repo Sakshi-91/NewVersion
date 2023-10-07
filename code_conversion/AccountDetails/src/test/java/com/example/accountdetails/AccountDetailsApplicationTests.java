f8c3ae3d096749b09d2667b30d371a7557ab6114
Sakshi-Git1 <sakshi.agraswal91@gmail.com>
2023-10-07 15:29:44 +0530

    chkedin

---
+++ code_conversion/AccountDetails/src/test/java/com/example/accountdetails/AccountDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
