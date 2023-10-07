c2934bd8b3f28be70cee8f4412c81a70c190b373
Sakshi-Git1 <144549342+Sakshi-Git1@users.noreply.github.com>
2023-10-07 16:31:31 +0530

    Update GetAccountDetailsApplication.java

--- code_conversion/GetAccountDetails/src/main/java/com/GetAccountDetails/GetAccountDetailsApplication.java
+++ code_conversion/GetAccountDetails/src/main/java/com/GetAccountDetails/GetAccountDetailsApplication.java
@@ -1,13 +1,9 @@
 package com.GetAccountDetails;
-​
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-​
 @SpringBootApplication
 public class GetAccountDetailsApplication {
-​
 	public static void main(String[] args) {
 		SpringApplication.run(GetAccountDetailsApplication.class, args);
 	}
-​
 }
