e2a0fa6c7ba23c6879b9e743f7be28476696c2cf
Sakshi-Git1 <144549342+Sakshi-Git1@users.noreply.github.com>
2023-10-07 13:59:02 +0530

    Update codeconvertor.sh

--- script/codeconvertor.sh
+++ script/codeconvertor.sh
@@ -30,13 +30,16 @@ repo_name=`echo $repo_url|awk -F'/' '{printf "%s",$NF}'`
 repo_name="./GetAccountDetails"
 echo $repo_name
 cd $repo_name
+#file_list=(`find . -name *.cs|grep -vE '/obj/Debug/|/obj/Release/|/Properties/|/obj/Debug/' && find . -name *.wsdl`)
 file_list=(`find . -name *.cs|grep -vE '/obj/Debug/|/obj/Release/|/Properties/|/obj/Debug/'`)
 file_content=`cat ${file_list[*]}|grep -v '//'|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
+repo_name=`echo ${repo_name}|tr '/' ' '|tr '.' ' '`
+repo_name=`echo ${repo_name}`
 cd ..
 echo ${file_list[*]}
-prompt=`cat ./script/prompt.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
+prompt=`cat ./script/prompt_main.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
+#prompt=`cat ./prompt/prompt_main.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
 echo $prompt
-
 ​
 mkdir -p code_conversion
 cd code_conversion
@@ -60,28 +63,53 @@ awk -v RS="\`\`\`java" -v ORS="" -v ARR="${file_names[*]}" 'BEGIN {split(ARR,FIL
 for i in *.java
 do
 	pure_code=$(cat $i |awk -v RS="\`\`\`" NR==1'{print $0}')
+	echo "$pure_code"|grep 'package com.example' >/dev/null && echo "$pure_code"|sed 's/com.example/com/g' > $i
 	echo "$pure_code"|grep 'package com' >/dev/null && echo "$pure_code" > $i
 	echo "$pure_code"|grep "package ${repo_name}" && echo "$pure_code"|sed 's/package /package com./g' > $i
+	#echo start
+	proj=`cat $i|grep '^package '|awk -v RS='.' '$1 ~ ";" {print $0}'|tr ';' ' '`
+	#echo ${proj}
+	#echo end
 	#echo "$pure_code" > $i
 done
+#echo ${proj} here
 awk -v RS="\`\`\`xml" -v ORS="" 'NR > 1 {print $0 > "pom.xml"}' <<< "$java_code"
 pom=$(cat pom.xml|awk -v RS="\`\`\`" 'NR==1 {print $0}')
-git_cmd=$(cat pom.xml|awk -v RS="\`\`\`" 'NR%2!=0 && NR!=1 {print $0}')
-echo "$git_cmd" > git.txt
-grep git git.txt
-if [ $? -ne 0 ]
-then
-	git_cmd=$(cat pom.xml|awk -v RS="\`\`\`" 'NR>1 {print $0}')
-	echo "$git_cmd"|awk -v FS="\`" '{print $2}'|grep '^git' > git.txt
-fi
+#git_cmd=$(cat pom.xml|awk -v RS="\`\`\`" 'NR%2!=0 && NR!=1 {print $0}')
+#echo "$git_cmd" > git.txt
+#grep git git.txt
+#if [ $? -ne 0 ]
+#then
+#	git_cmd=$(cat pom.xml|awk -v RS="\`\`\`" 'NR>1 {print $0}')
+#	echo "$git_cmd"|awk -v FS="\`" '{print $2}'|grep '^git' > git.txt
+#fi
 echo "$pom" > pom.xml
 #mv *.java ../AccountDetails/src/main/java/com/example/accountdetails/
 #mv pom.xml ../AccountDetails/
-proj=${repo_name}
-art=${proj}
+#proj=${repo_name}
+#art=${proj}
+proj=`echo ${proj}`
 grp=com
-spring init --build=maven --dependencies=web --group=${grp} --artifact-id=${art} --package-name=${grp}.${art} --name=${proj} ${proj}
-mv *.java ./${proj}/src/main/java/${grp}/${proj}/
+echo ${proj} here
+#spring init --build=maven --dependencies=web --group=${grp} --artifact-id=${art} --package-name=${grp}.${art} --name=${proj} ${proj}
+cat << EOF > ${proj}Application.java
+package com.${proj};
+​
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+​
+@SpringBootApplication
+public class ${proj}Application {
+​
+	public static void main(String[] args) {
+		SpringApplication.run(${proj}Application.class, args);
+	}
+​
+}
+EOF
+mkdir -p "./${proj}/src/main/java/${grp}/${proj}/"
+mv *.java "./${proj}/src/main/java/${grp}/${proj}/"
+mv pom.xml "./${proj}/"
 #spring init --build=maven --dependencies=web  ${repo_name}_java
 #bash git.txt
 ​
