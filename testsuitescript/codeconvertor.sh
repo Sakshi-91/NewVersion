15eb65a5f9c5f8fe10f2ac76dda117e44afe7183
Sakshi-Git1 <144549342+Sakshi-Git1@users.noreply.github.com>
2023-10-10 01:29:29 +0530

    Update codeconvertor.sh

--- testsuitescript/codeconvertor.sh
+++ testsuitescript/codeconvertor.sh
@@ -21,7 +21,8 @@ curl --silent $url \
 url=https://api.openai.com/v1/chat/completions
 repo_name="./GetAccountDetails"
 cd $repo_name
-file_list=(`find . -name *.java && find ../testsuitescript -name *.xml`)
+#file_list=(`find . -name *.java && find ../testsuitescript -name *.xml`)
+file_list=(`find . -name *.java`)
 echo ${file_list[*]}
 file_content=`cat ${file_list[*]}|grep -v '//'|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
 cd ..
