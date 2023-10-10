c97d693f9a2b8e387de1604ec536bd554e810c6c
amanrajputibm <136298973+amanrajputibm@users.noreply.github.com>
2023-10-10 14:22:40 +0530

    Update codeconvertor.sh
    code conversion

--- script/codeconvertor.sh
+++ script/codeconvertor.sh
@@ -28,7 +28,7 @@ url=https://api.openai.com/v1/chat/completions
 repo_url=$1
 repo_name=`echo $repo_url|awk -F'/' '{printf "%s",$NF}'`
 repo_name="./GetAccountDetails"
-echo $repo_name
+#echo $repo_name
 cd $repo_name
 #file_list=(`find . -name *.cs|grep -vE '/obj/Debug/|/obj/Release/|/Properties/|/obj/Debug/' && find . -name *.wsdl`)
 file_list=(`find . -name *.cs|grep -vE '/obj/Debug/|/obj/Release/|/Properties/|/obj/Debug/'`)
@@ -36,10 +36,10 @@ file_content=`cat ${file_list[*]}|grep -v '//'|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
 repo_name=`echo ${repo_name}|tr '/' ' '|tr '.' ' '`
 repo_name=`echo ${repo_name}`
 cd ..
-echo ${file_list[*]}
+#echo ${file_list[*]}
 prompt=`cat ./script/prompt.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
 #prompt=`cat ./prompt/prompt_main.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
-echo $prompt
+#echo $prompt
 ​
 mkdir -p code_conversion
 cd code_conversion
@@ -55,30 +55,31 @@ java_code="${java_code//'\n'/$'\n'}"
 #echo "$(java_code)"|tr '\\n' '\n' > input.json
 # Create directory for Java files
 file_names=$(grep -F .java: <<< "$java_code"|tr : ' '|cut -d. -f1)
-echo ${file_names[*]}
+#echo ${file_names[*]}
 # Use awk to split Java code into individual files
 indx=0
-awk -v RS="\`\`\`java" -v ORS="" -v ARR="${file_names[*]}" 'BEGIN {split(ARR,FILE_NAME," ")} NR > 1 {print $0 > FILE_NAME[NR-1] ".java"}' <<< "$java_code"
+#awk -v RS="\`\`\`java" -v ORS="" -v ARR="${file_names[*]}" 'BEGIN {split(ARR,FILE_NAME," ")} NR > 1 {print $0 > FILE_NAME[NR-1] ".java"}' <<< "$java_code"
+awk -v RS="\`\`\`java" -v ORS="" -v ARR="${file_names[*]}" 'BEGIN {split(ARR,FILE_NAME," ")} NR > 1 && NR < 6 {print $0 }' <<< "$java_code"
 #cd java_files
-for i in *.java
-do
-	pure_code=$(cat $i |awk -v RS="\`\`\`" NR==1'{print $0}')
-	#echo "$pure_code"|grep 'package com.example' >/dev/null && echo "$pure_code"|sed 's/com.example/com/g' > $i
+#for i in *.java
+#do
+#	pure_code=$(cat $i |awk -v RS="\`\`\`" NR==1'{print $0}')
+	##echo "$pure_code"|grep 'package com.example' >/dev/null && echo "$pure_code"|sed 's/com.example/com/g' > $i
 	#echo "$pure_code"|grep 'package com' >/dev/null && echo "$pure_code" > $i
 	#echo "$pure_code"|grep "package ${repo_name}" && echo "$pure_code"|sed 's/package /package com./g' > $i
-	echo "$pure_code"|grep 'package com.example' >/dev/null && echo "$pure_code"|sed 's/com.example/com/g'
-	echo "$pure_code"|grep 'package com' >/dev/null && echo "$pure_code"
-	echo "$pure_code"|grep "package ${repo_name}" && echo "$pure_code"|sed 's/package /package com./g'
+#	echo "$pure_code"|grep 'package com.example' >/dev/null && echo "$pure_code"|sed 's/com.example/com/g'
+#	echo "$pure_code"|grep 'package com' >/dev/null && echo "$pure_code"
+#	echo "$pure_code"|grep "package ${repo_name}" && echo "$pure_code"|sed 's/package /package com./g'
 		
  #echo start
-	proj=`cat $i|grep '^package '|awk -v RS='.' '$1 ~ ";" {print $0}'|tr ';' ' '`
+#	proj=`cat $i|grep '^package '|awk -v RS='.' '$1 ~ ";" {print $0}'|tr ';' ' '`
 	#echo ${proj}
 	#echo end
 	#echo "$pure_code" > $i
-done
+#done
 #echo ${proj} here
-awk -v RS="\`\`\`xml" -v ORS="" 'NR > 1 {print $0 > "pom.xml"}' <<< "$java_code"
-pom=$(cat pom.xml|awk -v RS="\`\`\`" 'NR==1 {print $0}')
+#awk -v RS="\`\`\`xml" -v ORS="" 'NR > 1 {print $0 > "pom.xml"}' <<< "$java_code"
+#pom=$(cat pom.xml|awk -v RS="\`\`\`" 'NR==1 {print $0}')
 #git_cmd=$(cat pom.xml|awk -v RS="\`\`\`" 'NR%2!=0 && NR!=1 {print $0}')
 #echo "$git_cmd" > git.txt
 #grep git git.txt
@@ -88,30 +89,30 @@ pom=$(cat pom.xml|awk -v RS="\`\`\`" 'NR==1 {print $0}')
 #	echo "$git_cmd"|awk -v FS="\`" '{print $2}'|grep '^git' > git.txt
 #fi
 #echo "$pom" > pom.xml
-echo "$pom" 
+#echo "$pom" 
 #mv *.java ../AccountDetails/src/main/java/com/example/accountdetails/
 #mv pom.xml ../AccountDetails/
 #proj=${repo_name}
 #art=${proj}
-proj=`echo ${proj}`
-grp=com
-echo ${proj} here
+#proj=`echo ${proj}`
+#grp=com
+#echo ${proj} here
 #spring init --build=maven --dependencies=web --group=${grp} --artifact-id=${art} --package-name=${grp}.${art} --name=${proj} ${proj}
-cat << EOF > ${proj}Application.java
-package com.${proj};
-​
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-​
-@SpringBootApplication
-public class ${proj}Application {
-​
-	public static void main(String[] args) {
-		SpringApplication.run(${proj}Application.class, args);
-	}
-​
-}
-EOF
+#cat << EOF > ${proj}Application.java
+#package com.${proj};
+#​
+#import org.springframework.boot.SpringApplication;
+##import org.springframework.boot.autoconfigure.SpringBootApplication;
+​#
+#@SpringBootApplication
+#public class ${proj}Application {
+#​
+#	public static void main(String[] args) {
+#		SpringApplication.run(${proj}Application.class, args);
+#	}
+#​
+#}
+#EOF
 #mkdir -p "./${proj}/src/main/java/${grp}/${proj}/"
 #mv *.java "./${proj}/src/main/java/${grp}/${proj}/"
 #mv pom.xml "./${proj}/"
