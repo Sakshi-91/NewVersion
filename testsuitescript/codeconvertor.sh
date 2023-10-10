31344cb21dd298b697e1c02ae05e0176983764ee
amanrajputibm <136298973+amanrajputibm@users.noreply.github.com>
2023-10-10 11:47:02 +0530

    Update codeconvertor.sh
    Updating the test suite script for printing result only on console.

--- testsuitescript/codeconvertor.sh
+++ testsuitescript/codeconvertor.sh
@@ -16,7 +16,6 @@ curl --silent $url \
 		     \"temperature\": 0.5
 	            }" 
 }
-
 #url=https://jsonplaceholder.typicode.com/posts
 url=https://api.openai.com/v1/chat/completions
 repo_name="./code_conversion/"
@@ -27,6 +26,6 @@ echo ${file_list[*]}
 file_content=`cat ${file_list[*]}|grep -v '//'|tr '\r\n' ' '|sed 's/\"/\\\"/g'`
 cd ../testsuitescript
 prompt=`cat ./prompt.txt|sed ':a;N;$!ba;s/\n/\\\n/g'|sed 's/\"/\\\"/g'`
-echo "${prompt}"
+#echo "${prompt}"
 #convert_code "$prompt" "$req_file" 
 convert_code "$prompt" "$file_content"|sed -e 's/\\n/\n/g' -e 's/\\\"/\"/g' -e 's/\\t/	/g'
